ce1be06e9f89a458c807a4e450b92617ff37b460
Aimee <Aimee@Amy>
2018-02-10 17:50:56 -0500

    y

--- src/ArrayMethods2.java
+++ src/ArrayMethods2.java
@@ -10,26 +10,28 @@ public class ArrayMethods2 {
 		String[] test2 = {"banana", "cherry", "mahogony", "oreos", "pasta"};
 		int[] test3 = {3, 4, 2, 7, 12, 22, 0, 1};	
 			
-		//merge test
+		/*//merge test
 		long start = System.nanoTime();
 		String[] mergeResult = merge (test1, test2);
 		long end = System.nanoTime();
 		long time = end - start;
 		System.out.println("Merge test took: " + time + " nanoseconds.");
 		System.out.println(Arrays.toString(mergeResult));
+		*/
 		
 		//Partition test
-		start = System.nanoTime();
+		long start = System.nanoTime();
 		int pivotFinalPos = partition(test3);
-		end = System.nanoTime();
-		time = end - start;
+		long end = System.nanoTime();
+		long time = end - start;
 		System.out.println("Partition test took: " + time + " nanoseconds.");
 		System.out.println("Final Pivot Position: " + pivotFinalPos);
 		System.out.println(Arrays.toString(test3));
 	}
 	
+
 	//CHANGE TO ALPHABETICAL ORDER
-	public static String[] merge(String[] list1, String[] list2) {
+	/*public static String[] merge(String[] list1, String[] list2) {
 		for (int i = 0; i < list1.length; i++) {
 			for (int j = 0; j < list2.length; j++) {
 				if (list1[i].length() < list2[j].length()) {
@@ -42,35 +44,47 @@ public class ArrayMethods2 {
 			}
 		}
 	}
-	
+	*/
 	/*
 	Write a method that partitions a list using the first element as the pivot. You should return the
 	integer position of the pivot in the newly partitioned array. The original array should be rearranged
 	so that all the elements before the pivot are less than or equal to the pivot and all the elements 
 	after the pivot are greater than the pivot.
 	*/
-	public static int partition(int[] list, int first, int next) {
+	
+	
+	
+	
+	//int pivot = int first
+	//int first = 0
+	//if pivot > pivot+1
+	//swap
+	//first++
+	public static int partition(int[] list) {
+		int first = 0;
 		int pivot = list[first];
-		next = pivot + 1;
-		for (int i = 0; i < list.length; i++) {
+		int next = list[first + 1];
+		while (list[pivot] != list.length-1) {
+			
 			if (pivot > next) {
-				//swap?
-				
+				swap (list, pivot, next); //no
+				first++;
 			}
 			if (pivot < next) {
 				next++;
 			}
 		}
-		/*while (first < last) {
-			
-			while (pivot > ) {
-				first++;
-			}
-			while (list[first] > pivot) {
-				first--;
-			}
-
-		}*/
+		return first;
 	}
+
+	
+	
+	
+	public static void swap (int arr[], int x, int y)
+	   {
+	      int swap = arr[x];
+	      arr[x] = arr[y];
+	      arr[y] = swap;
+	   }
 }
 
